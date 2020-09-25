2c7b57ce2dc6d3959d0cfe6a3e7ce995295a2b0b
udasitharani <udasitharani@gmail.com>
2020-09-26 01:30:02 +0530

    onboard section

--- lib/screens/Home/Home.dart
+++ lib/screens/Home/Home.dart
@@ -10,8 +10,47 @@ class Home extends StatelessWidget {
       body: Column(
         children: [
           Carousel(),
+          SizedBox(height: 20.0),
+          OnBoardSection(),
         ],
       ),
+    );
+  }
+}
+
+class OnBoardSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Free shipping over \$30",
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            decoration: TextDecoration.underline,
+            decorationColor: Colors.yellow[700],
+            decorationThickness: 2.0,
+          ),
+        ),
+        SizedBox(height: 20.0),
+        FlatButton(
+          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          onPressed: () {},
+          child: Text(
+            "SIGN IN",
+            style: TextStyle(fontSize: 10.0, color: Colors.white),
+          ),
+          color: Colors.blue[400],
+        ),
+        FlatButton(
+          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          onPressed: () {},
+          child: Text(
+            'New account',
+            style: TextStyle(fontSize: 9.0, color: Colors.blue[400]),
+          ),
+        ),
+      ],
     );
   }
 }
